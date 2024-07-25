19ab02ea2cccfe7c8545876147e8360f2e3b6d82
faeemazaz <faeemazaz.bhanej92@gmail.com>
2024-07-25 11:52:37 +0530

    initial commit

---
+++ src/main/java/home/meal/hub/HomeMealHubApplication.java
@@ -0,0 +1,13 @@
+package home.meal.hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMealHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeMealHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/meal/hub/HomeMealHubApplicationTests.java
@@ -0,0 +1,13 @@
+package home.meal.hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeMealHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
